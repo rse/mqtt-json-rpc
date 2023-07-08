15ea9c39f64d4637c13dec9b22bf524ecdaf7c7e
Ralf S. Engelschall <rse@engelschall.com>
2023-07-08 13:20:13 +0200

    provide TypeScript declaration file

---
+++ mqtt-json-rpc.d.ts
@@ -0,0 +1,54 @@
+/*
+**  MQTT-JSON-RPC -- JSON-RPC protocol over MQTT communication
+**  Copyright (c) 2018-2023 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+declare module "mqtt-json-rpc" {
+    export class API {
+        constructor(
+            mqtt:          any,
+            options: {
+                encoding?: string,
+                timeout?:  number
+            }
+        )
+        registered(
+            method:        string
+        ):                 boolean
+        register(
+            method:        string,
+            callback:      (...params: any[]) => any
+        ):                 Promise<boolean>
+        unregister(
+            method:        string
+        ):                 Promise<any>
+        notify (
+            method:        string,
+            ...params:     any[]
+        ):                 void
+        call (
+            method:        string,
+            ...params:     any[]
+        ):                 Promise<any>
+    }
+}
+
